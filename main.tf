45f6eb8ce9d08ab44b6206d056b682d11bd115ea
melophe <sunlavins@gmail.com>
2025-07-28 21:29:00 +0900

    07-28

---
+++ main.tf
@@ -0,0 +1,37 @@
+# Terraform configuration
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">3.0"
+    }
+  }
+}
+
+#provider
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+#variables
+variable "project" {
+  type        = string
+  description = "name"
+}
+
+variable "environment" {
+  type        = string
+  description = "name"
+}
+
+output "project" {
+  value = var.project
+}
+
+output "environment" {
+  value       = var.environment
+  description = "test"
+  sensitive   = false
+}
